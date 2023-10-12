aac2ebdf3df6fc31bf5b856179a3e051c2185b04
Gabriel Souza <gabrielsouzadevweb@gmail.com>
2023-10-12 20:44:51 -0300

    Initial commit

---
+++ apps/ps-demo-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ps-demo-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ps-demo-app';
+}
